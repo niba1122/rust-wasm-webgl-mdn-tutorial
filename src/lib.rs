c7e9a50e913e468ed7d808cde2d854239313f8c1
Nobuhito Ibaraki <nobuhito.ibaraki@zozo.com>
2019-11-26 14:27:01 +0900

    WebGL tutorial: https://developer.mozilla.org/ja/docs/Web/API/WebGL_API/Tutorial/Getting_started_with_WebGL

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ mod utils;
 
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
+use web_sys::{WebGlRenderingContext};
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -10,6 +11,44 @@ use wasm_bindgen::JsCast;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+
+
+#[wasm_bindgen]
 pub fn start() -> Result<(), JsValue> {
+    let context = get_webgl_context_by_id("canvas");
+
+    context.clear_color(0.0, 0.0, 0.0, 1.0);
+    context.enable(WebGlRenderingContext::DEPTH_TEST);
+    context.depth_func(WebGlRenderingContext::LEQUAL);
+    context.clear(WebGlRenderingContext::COLOR_BUFFER_BIT | WebGlRenderingContext::DEPTH_BUFFER_BIT);
+
     Ok(())
+}
+
+fn get_webgl_context_by_id(id: &str) -> WebGlRenderingContext {
+    let document = web_sys::window()
+        .unwrap()
+        .document()
+        .unwrap();
+
+    let canvas = document
+        .get_element_by_id(id)
+        .unwrap()
+        .dyn_into::<web_sys::HtmlCanvasElement>()
+        .unwrap();
+
+    let context = canvas
+        .get_context("webgl")
+        .unwrap()
+        .unwrap()
+        .dyn_into::<WebGlRenderingContext>()
+        .unwrap();
+
+    context.viewport(0, 0, canvas.width() as i32, canvas.height() as i32);
+
+    context
 }
