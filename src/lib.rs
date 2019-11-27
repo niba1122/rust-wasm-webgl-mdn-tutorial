0ec9a57a88b161321c63ddc7c07c325828131c15
Nobuhito Ibaraki <nobuhito.ibaraki@zozo.com>
2019-11-27 11:04:11 +0900

    Fix error: https://developer.mozilla.org/ja/docs/Web/API/WebGL_API/Tutorial/Adding_2D_content_to_a_WebGL_context

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ mod utils;
 
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
-use web_sys::{WebGlRenderingContext, WebGlShader};
+use web_sys::{WebGl2RenderingContext, WebGlShader, WebGlBuffer, WebGlProgram, WebGlUniformLocation};
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -18,8 +18,8 @@ extern "C" {
     #[wasm_bindgen(js_namespace = console)]
     fn error(s: &str);
 
-    #[wasm_bindgen(js_namespace = mat4, js_name = create)]
-    fn mat4_create() -> js_sys::Float32Array;
+    #[wasm_bindgen]
+    fn calc(field_of_view: f32, aspect: f32, z_near: f32, z_far: f32) -> js_sys::Object;
 }
 
 static FRAGMENT_SHADER: &'static str = r#"
@@ -31,11 +31,11 @@ static FRAGMENT_SHADER: &'static str = r#"
 static VERTEX_SHADER: &'static str = r#"
     attribute vec3 aVertexPosition;
 
-    uniform mat4 uMVMatrix;
-    uniform mat4 uPMatrix;
+    uniform mat4 uModelViewMatrix;
+    uniform mat4 uProjectionMatrix;
 
     void main(void) {
-        gl_Position = uPMatrix * uMVMatrix * vec4(aVertexPosition, 1.0);
+        gl_Position = uProjectionMatrix * uModelViewMatrix * vec4(aVertexPosition, 1.0);
     }
 "#;
 
@@ -44,16 +44,20 @@ static VERTEX_SHADER: &'static str = r#"
 pub fn start() -> Result<(), JsValue> {
     let context = get_webgl_context_by_id("canvas");
 
-    init_shaders(&context);
+    let shader_program = init_shaders(&context);
 
-    init_buffers(&context);
+    let position_buffer = init_buffers(&context);
 
-    draw_scene(&context);
+    draw_scene(
+        &context,
+        &position_buffer,
+        &shader_program,
+    );
 
     Ok(())
 }
 
-fn get_webgl_context_by_id(id: &str) -> WebGlRenderingContext {
+fn get_webgl_context_by_id(id: &str) -> WebGl2RenderingContext {
     let document = web_sys::window()
         .unwrap()
         .document()
@@ -66,10 +70,10 @@ fn get_webgl_context_by_id(id: &str) -> WebGlRenderingContext {
         .unwrap();
 
     let context = canvas
-        .get_context("webgl")
+        .get_context("webgl2")
         .unwrap()
         .unwrap()
-        .dyn_into::<WebGlRenderingContext>()
+        .dyn_into::<WebGl2RenderingContext>()
         .unwrap();
 
     context.viewport(0, 0, canvas.width() as i32, canvas.height() as i32);
@@ -77,28 +81,28 @@ fn get_webgl_context_by_id(id: &str) -> WebGlRenderingContext {
     context
 }
 
-fn get_shader(context: &WebGlRenderingContext, shader_type: u32, source: &str) -> WebGlShader {
+fn get_shader(context: &WebGl2RenderingContext, shader_type: u32, source: &str) -> WebGlShader {
     let shader = context.create_shader(shader_type).unwrap();
 
     context.shader_source(&shader, source);
     context.compile_shader(&shader);
-    let compile_is_succeeded = context.get_shader_parameter(&shader, WebGlRenderingContext::COMPILE_STATUS).as_bool().unwrap();
+    let compile_is_succeeded = context.get_shader_parameter(&shader, WebGl2RenderingContext::COMPILE_STATUS).as_bool().unwrap();
     if !compile_is_succeeded {
         panic!("シェーダーのコンパイルでエラーが発生しました");
     }
     shader
 }
 
-fn init_shaders(context: &WebGlRenderingContext) {
-    let fragment_shader = get_shader(&context, WebGlRenderingContext::FRAGMENT_SHADER, FRAGMENT_SHADER);
-    let vertex_shader = get_shader(&context, WebGlRenderingContext::VERTEX_SHADER, VERTEX_SHADER);
+fn init_shaders(context: &WebGl2RenderingContext) -> WebGlProgram {
+    let fragment_shader = get_shader(&context, WebGl2RenderingContext::FRAGMENT_SHADER, FRAGMENT_SHADER);
+    let vertex_shader = get_shader(&context, WebGl2RenderingContext::VERTEX_SHADER, VERTEX_SHADER);
 
     let shader_program = context.create_program().unwrap();
     context.attach_shader(&shader_program, &vertex_shader);
     context.attach_shader(&shader_program, &fragment_shader);
     context.link_program(&shader_program);
 
-    let shader_is_created = context.get_program_parameter(&shader_program, WebGlRenderingContext::LINK_STATUS).as_bool().unwrap();
+    let shader_is_created = context.get_program_parameter(&shader_program, WebGl2RenderingContext::LINK_STATUS).as_bool().unwrap();
 
     if !shader_is_created {
         let info = context.get_program_info_log(&shader_program).unwrap();
@@ -109,38 +113,89 @@ fn init_shaders(context: &WebGlRenderingContext) {
 
     let vertex_position_attribute = context.get_attrib_location(&shader_program, "aVertexPosition");
     context.enable_vertex_attrib_array(vertex_position_attribute as u32);
+
+    shader_program
 }
 
 // var horizAspect = 480.0/640.0;
 
-fn init_buffers(context: &WebGlRenderingContext) {
-    let square_vertices_buffer = context.create_buffer().unwrap();
-    context.bind_buffer(WebGlRenderingContext::ARRAY_BUFFER, Some(&square_vertices_buffer));
+fn init_buffers(context: &WebGl2RenderingContext) -> WebGlBuffer {
+    let position_buffer = context.create_buffer().unwrap();
+    context.bind_buffer(WebGl2RenderingContext::ARRAY_BUFFER, Some(&position_buffer));
     
     let vertices: &[f32] = &[
-        1.0,  1.0,  0.0,
-        -1.0, 1.0,  0.0,
-        1.0,  -1.0, 0.0,
-        -1.0, -1.0, 0.0
+        1.0,  1.0,
+        -1.0, 1.0,
+        1.0,  -1.0,
+        -1.0, -1.0
     ];
   
     unsafe {
-        context.buffer_data_with_array_buffer_view(WebGlRenderingContext::ARRAY_BUFFER, &js_sys::Float32Array::view(vertices), WebGlRenderingContext::STATIC_DRAW);
+        context.buffer_data_with_array_buffer_view(WebGl2RenderingContext::ARRAY_BUFFER, &js_sys::Float32Array::view(vertices), WebGl2RenderingContext::STATIC_DRAW);
     }
+
+    position_buffer
 }
 
-fn draw_scene(context: &WebGlRenderingContext) {
+fn draw_scene(
+    context: &WebGl2RenderingContext,
+    position_buffer: &WebGlBuffer,
+    shader_program: &WebGlProgram,
+) {
     context.clear_color(0.0, 0.0, 0.0, 1.0);
-    context.enable(WebGlRenderingContext::DEPTH_TEST);
-    context.depth_func(WebGlRenderingContext::LEQUAL);
-    context.clear(WebGlRenderingContext::COLOR_BUFFER_BIT | WebGlRenderingContext::DEPTH_BUFFER_BIT);
+    context.enable(WebGl2RenderingContext::DEPTH_TEST);
+    context.depth_func(WebGl2RenderingContext::LEQUAL);
+    context.clear(WebGl2RenderingContext::COLOR_BUFFER_BIT | WebGl2RenderingContext::DEPTH_BUFFER_BIT);
 
     let canvas = context.canvas().unwrap().dyn_into::<web_sys::HtmlCanvasElement>().unwrap();
 
-    let mat4 = mat4_create();
-    // let fieldOfView = 45.0 * std::f64::consts::PI / 180.0;
-    // let aspect = canvas.client_width() / canvas.client_height();
-    // let z_near = 0.1;
-    // let z_far = 100.0;
-    // let projectionMatrix = mat4_create();
+    let field_of_view = 45.0 * std::f32::consts::PI / 180.0;
+    let aspect = canvas.client_width() as f32 / canvas.client_height() as f32;
+    let z_near = 0.1;
+    let z_far = 100.0;
+
+    let result = calc(field_of_view, aspect, z_near, z_far);
+    let raw_projection_matrix: js_sys::Float32Array = js_sys::Reflect::get(&result, &js_sys::JsString::from("projectionMatrix")).unwrap().into();
+    let projection_matrix = js_sys::Float32Array::from(raw_projection_matrix).to_vec();
+
+    let raw_model_view_matrix: js_sys::Float32Array = js_sys::Reflect::get(&result, &js_sys::JsString::from("modelViewMatrix")).unwrap().into();
+    let model_view_matrix = js_sys::Float32Array::from(raw_model_view_matrix).to_vec();
+
+    let num_components = 2;
+    let rendering_type: u32 = WebGl2RenderingContext::FLOAT;
+    let normalize = false;
+    let stride = 0;
+    let offset = 0;
+
+    let vertex_position = context.get_attrib_location(&shader_program, "aVertexPosition") as u32;
+    context.bind_buffer(WebGl2RenderingContext::ARRAY_BUFFER, Some(&position_buffer));
+    context.vertex_attrib_pointer_with_i32(
+        vertex_position,
+        num_components,
+        rendering_type,
+        normalize,
+        stride,
+        offset
+    );
+    context.enable_vertex_attrib_array(vertex_position);
+    context.use_program(Some(&shader_program));
+
+    let program_projection_matrix = context.get_uniform_location(&shader_program, "uProjectionMatrix").unwrap();
+    context.uniform_matrix4fv_with_f32_array(
+        Some(&program_projection_matrix),
+        false,
+        &projection_matrix
+    );
+
+    let program_model_view_matrix = context.get_uniform_location(&shader_program, "uModelViewMatrix").unwrap();
+    context.uniform_matrix4fv_with_f32_array(
+        Some(&program_model_view_matrix),
+        false,
+        &model_view_matrix
+    );
+
+    let offset = 0;
+    let vertex_count = 4;
+    context.draw_arrays(WebGl2RenderingContext::TRIANGLE_STRIP, offset, vertex_count);
 }
+
--- www/index.js
+++ www/index.js
@@ -1,3 +1,33 @@
 import * as wasm from "rust-wasm-fem";
 
+window.calc = function (fieldOfView, aspect, zNear, zFar) {
+
+  console.log(fieldOfView)
+  const projectionMatrix = mat4.create();
+
+  // note: glmatrix.js always has the first argument
+  // as the destination to receive the result.
+  mat4.perspective(projectionMatrix,
+                  fieldOfView,
+                  aspect,
+                  zNear,
+                  zFar);
+
+  // Set the drawing position to the "identity" point, which is
+  // the center of the scene.
+  const modelViewMatrix = mat4.create();
+
+  // Now move the drawing position a bit to where we want to
+  // start drawing the square.
+
+  mat4.translate(modelViewMatrix,     // destination matrix
+                modelViewMatrix,     // matrix to translate
+                [-0.0, 0.0, -6.0]);  // amount to translate
+
+  return {
+    projectionMatrix,
+    modelViewMatrix
+  }
+}
+
 wasm.start();
